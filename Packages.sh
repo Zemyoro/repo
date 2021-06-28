90e7c2be2f1f87d1ce2864568146183852d52aff
Zemyoro <85952603+Zemyoro@users.noreply.github.com>
2021-06-28 14:58:20 +0900

    Updated Packages.sh

--- Packages.sh
+++ Packages.sh
@@ -1,12 +1,17 @@
 echo Removing old compressed packages...
+sleep 2
 rm Packages.bz2
 rm Packages.Izma
 rm Packages.xz
 rm Packages.zst
 sleep 2
+echo ----------------------
 echo Compressing Packages!
+sleep 2
 7z a Packages.bz2 Packages
 7z a Packages.Izma Packages
 7z a Packages.xz Packages
 7z a Packages.zst Packages
+sleep 2
+echo ----------
 echo Finished!
